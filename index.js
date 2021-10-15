681b8fa4a4352a7f169ce343ea9d062adc32d28f
Andres Carrero <andres_car098@hotmail.com>
2021-10-15 09:34:11 -0500

    GET: parametros query

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 3000;
 
@@ -11,16 +12,22 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Producto 1',
-      precio: 1000,
-    },
-    {
-      name: 'Producto 2',
-      precio: 2000,
-    },
-  ]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
+});
+
+app.get('/products/filter', (req, res) => {
+  res.send('Este es un filtro');
 });
 
 app.get('/products/:id', (req, res) => {
@@ -30,6 +37,18 @@ app.get('/products/:id', (req, res) => {
     name: 'Producto 1',
     precio: 1000,
   });
+});
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay parametros query');
+  }
 });
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
