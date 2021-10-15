f1309a1bc9df34de90440025e4e0c182e367c4bb
Andres Carrero <andres_car098@hotmail.com>
2021-10-15 08:50:48 -0500

    Instalacion de Express.js y primer servidor HTTP

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("My App");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Servidor de express');
+});
+
+app.listen(port, () => {
+  console.log(`Ejecutando en puerto ${port}`);
+});
